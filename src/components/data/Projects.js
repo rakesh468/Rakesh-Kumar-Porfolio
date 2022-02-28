b7d3d1eeef7be909e756f313b2440e86fc8b7e75
rakesh468 <rakeshkumarg2000@gmail.com>
2022-02-28 08:49:20 -0800

    first commit

--- src/components/data/Projects.js
+++ src/components/data/Projects.js
@@ -1,39 +1,39 @@
 export const ProjectData=[
     {
         id:1,
-        title:"Airbnb Clone App",
+        title:"AIRBNB CLONE APP",
         description:"Airbnb Clone App is made using reactjs and nodejs.CRUD operation implemented in the app,where rooms can be created,edited,updated and read",
         tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
         forntend:"https://github.com/rakesh468/Airbnb-Forntend",
         backend:"https://github.com/rakesh468/airbnb-backend",
-        netlify:"https://blissful-ramanujan-c7caee.netlify.app",
+        netlify:"https://fervent-northcutt-81a5ec.netlify.app",
         image:"https://images.pexels.com/photos/2659629/pexels-photo-2659629.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
     },
     {
         id:2,
-        title:"Movies CRUD App",
+        title:"MOVIES CRUD APP",
         description:"Simple movie app is made using nodejs and reactjs. CRUD operations implemented for the list of movies, where movies can be deleted, updated, created and read.",
         tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
         forntend:"https://github.com/rakesh468/Movie-App",
         backend:"https://github.com/rakesh468/Movie-App-BackEnd",
-        netlify:"https://thirsty-yonath-3fb8e8.netlify.app",
+        netlify:"https://clever-visvesvaraya-075583.netlify.app",
         image:"https://beebom.com/wp-content/uploads/2019/07/Plex.jpg"
     },{
         id:3,
-        title:"Blog App",
+        title:"BLOG APP",
         description:"Blog application App is made using reactjs and nodejs.CRUD operation implemented for the app,To Create,Delete,Update and read",
         tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
         forntend:"https://github.com/rakesh468/Blog-Forntend",
         backend:"https://github.com/rakesh468/Blog-Backend",
-        netlify:"https://unruffled-cray-817b7b.netlify.app",
+        netlify:"https://stoic-bose-24d094.netlify.app",
         image:"https://patthomson.files.wordpress.com/2017/08/img_1638.png?w=640"
        
 
     },
     {
         id:4,
-        title:"E-Commerce App",
-        description:"E-Commerce App is made using Reactjs nd Nodejs.Crud operation implemented for the app,To Create product,Edit product,Update Product, Delete Product and Read",
+        title:"USERS CRUD APP",
+        description:"Simple user app is made using Reactjs and Nodejs. CRUD operations implemented for the list of users, where users can be deleted, updated, created and read.",
         tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
         forntend:"https://github.com/rakesh468/E-Commerce-App-Fontend",
         backend:"https://github.com/rakesh468/E-Commerce-App-Backend",

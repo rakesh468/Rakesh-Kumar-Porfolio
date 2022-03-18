3d9bb54c9a063f756b65cee86cd757ec3c70e2c4
rakesh468 <rakeshkumarg2000@gmail.com>
2022-03-18 07:09:48 -0700

    first commit

--- src/components/data/Projects.js
+++ src/components/data/Projects.js
@@ -1,49 +1,50 @@
-export const ProjectData=[
-    {
-        id:1,
-        title:"E-Commerce APP",
-        description:"Ecommerce website built with the MERN stack with React context API for state management ,pure css for style and CRUD operation implemented for the admin to crete,update,edit and delete products",
-        tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
-        forntend:"https://github.com/rakesh468/e-commerce",
-        backend:"https://github.com/rakesh468/e-commerce",
-        netlify:"https://e-commerce-sites.herokuapp.com",
-        image:"https://media.istockphoto.com/photos/shopping-online-concept-shopping-service-on-the-online-web-with-by-picture-id1133980246?k=20&m=1133980246&s=612x612&w=0&h=bwut2YUV7gtnjrv354523xU_9S-TtKQOqGTdiGMsPfs="
-      
-    },
-    {
-        id:2,
-        title:"MOVIES CRUD APP",
-        description:"Simple movie app is made using nodejs and reactjs. CRUD operations implemented for the list of movies, where movies can be deleted, updated, created and read.",
-        tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
-        forntend:"https://github.com/rakesh468/Movie-App",
-        backend:"https://github.com/rakesh468/Movie-App-BackEnd",
-        netlify:"https://clever-visvesvaraya-075583.netlify.app",
-        image:"https://www.maketecheasier.com/assets/uploads/2021/04/watch-movies-online-with-friends-parsec.jpg"
-    },{
-        id:3,
-        title:"BLOG APP",
-        description:"Blog application App is made using reactjs and nodejs.CRUD operation implemented for the app,To Create,Delete,Update and read",
-        tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
-        forntend:"https://github.com/rakesh468/Blog-Forntend",
-        backend:"https://github.com/rakesh468/Blog-Backend",
-        netlify:"https://stoic-bose-24d094.netlify.app",
-        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjL4WYFe53NRYP8SKwlbD4t2SdYLEtQAc0gQ&usqp=CAU"
-       
-
-    },
-    {
-        id:4,
-        title:"Airbnb App",
-        description:"Airbnb Clone App is made using reactjs and nodejs.CRUD operation implemented for the app.To Create Update,Read and Delete",
-        tags:["ReactJs","NodeJs","MongoDB","ExpressJs"],
-        forntend:"https://github.com/rakesh468/Airbnb-App",
-        backend:"https://github.com/rakesh468/Airbnb-App",
-        netlify:"https://airbnb-site-clone-app.herokuapp.com",
-        image:"https://static01.nyt.com/images/2016/07/24/travel/24Getaway-Hotels-vs-Airbnb/24Getaway-Hotels-vs-Airbnb-jumbo.jpg"
-    }
-    
-
-]
-
-
-
+export const ProjectData = [
+  {
+    id: 1,
+    title: "E-Commerce APP",
+    description:
+      "Ecommerce website built with the MERN stack with React context API for state management ,pure css for style and CRUD operation implemented for the admin to crete,update,edit and delete products",
+    tags: ["ReactJs", "NodeJs", "MongoDB", "ExpressJs"],
+    forntend: "https://github.com/rakesh468/e-commerce",
+    backend: "https://github.com/rakesh468/e-commerce",
+    netlify: "https://e-commerce-sites.herokuapp.com",
+    image:
+      "https://media.istockphoto.com/photos/shopping-online-concept-shopping-service-on-the-online-web-with-by-picture-id1133980246?k=20&m=1133980246&s=612x612&w=0&h=bwut2YUV7gtnjrv354523xU_9S-TtKQOqGTdiGMsPfs=",
+  },
+  {
+    id: 2,
+    title: "Airbnb Clone App",
+    description:
+      "Airbnb Clone App is made using reactjs and nodejs.CRUD operation implemented for the app.To Create Update,Read and Delete",
+    tags: ["ReactJs", "NodeJs", "MongoDB", "ExpressJs"],
+    forntend: "https://github.com/rakesh468/Airbnb-App",
+    backend: "https://github.com/rakesh468/Airbnb-App",
+    netlify: "https://airbnb-site-clone-app.herokuapp.com",
+    image:
+      "https://static01.nyt.com/images/2016/07/24/travel/24Getaway-Hotels-vs-Airbnb/24Getaway-Hotels-vs-Airbnb-jumbo.jpg",
+  },
+  {
+    id: 3,
+    title: "BLOG APP",
+    description:
+      "Blog application App is made using reactjs and nodejs.CRUD operation implemented for the app,To Create,Delete,Update and read",
+    tags: ["ReactJs", "NodeJs", "MongoDB", "ExpressJs"],
+    forntend: "https://github.com/rakesh468/Blog-Forntend",
+    backend: "https://github.com/rakesh468/Blog-Backend",
+    netlify: "https://stoic-bose-24d094.netlify.app",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjL4WYFe53NRYP8SKwlbD4t2SdYLEtQAc0gQ&usqp=CAU",
+  },
+  {
+    id: 4,
+    title: "MOVIES CRUD APP",
+    description:
+      "Simple movie app is made using nodejs and reactjs. CRUD operations implemented for the list of movies, where movies can be deleted, updated, created and read.",
+    tags: ["ReactJs", "NodeJs", "MongoDB", "ExpressJs"],
+    forntend: "https://github.com/rakesh468/Movie-App",
+    backend: "https://github.com/rakesh468/Movie-App-BackEnd",
+    netlify: "https://clever-visvesvaraya-075583.netlify.app",
+    image:
+      "https://www.maketecheasier.com/assets/uploads/2021/04/watch-movies-online-with-friends-parsec.jpg",
+  },
+];
